6aaa6b0646bf0c42e4d9c0e7a323779d16e76299
Crom Borw <66833938+CromBorw2e36@users.noreply.github.com>
2022-07-21 15:10:29 +0700

    chinhsua

--- inc/head.php
+++ inc/head.php
@@ -18,7 +18,7 @@ require_once($filepath . "../../vendor/autoload.php");
     <meta property="og:image" content="">
     <title><?php if (isset($title)) echo $title;
             else echo "Không tiêu đề" ?></title>
-
+    <link rel="icon" href="../public/images/icons/LogoGiaSu.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
