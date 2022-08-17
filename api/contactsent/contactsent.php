b13fb180ead4ff57ac1dd76ee2c8c67fde2bbb25
CromBorw2e36 <khanhdthucntt19@gmail.com>
2022-08-17 07:12:24 +0700

    contact - gửi dữ liệu về database

--- api/contactsent/contactsent.php
+++ api/contactsent/contactsent.php
@@ -5,15 +5,15 @@ namespace Ajax;
 use Exception;
 use Helpers\Format;
 use Library\Session;
-use Classes\SavedTutor;
+use Classes\Contact;
 
 $filepath  = realpath(dirname(__FILE__));
 
 require_once(__DIR__ . "../../../vendor/autoload.php");
 // include_once($filepath . "../../lib/session.php");
-if (!Session::checkRoles(['user', 'tutor'])) {
-        header("location:../../pages/errors/404");
-}
+// if (!Session::checkRoles(['user', 'tutor'])) {
+//         header("location:../../pages/errors/404");
+// }
 // include_once($filepath . "../../classes/savedtutors.php");
 // include_once($filepath . "../../helpers/format.php");
 Session::init();
@@ -22,21 +22,17 @@ Session::init();
 
 
 
-if (isset($_POST["tutorId"]) && !empty($_POST["tutorId"])) {
+if (isset($_POST["fullname"]) ) {
         try {
-                $tutorId = Format::validation($_POST["tutorId"]);
-                $save_tutor = new SavedTutor();
-                $isSavedTutor = $save_tutor->countTutorSaved(Session::get("userId"), $tutorId)->fetch_assoc()["numTutor"];
+                $fullname = Format::validation($_POST["fullname"]);
+                $email = Format::validation($_POST["email"]);
+                $phone = Format::validation($_POST["phone"]);
+                $content = Format::validation($_POST["content"]);
 
-                if ($isSavedTutor == 0) {
-                        $result = $save_tutor->createTutorSaved(Session::get("userId"), $tutorId);
+                $contact  = new contact();
+                
+                $contact->insertcontact($fullname,$email, $phone, $content);
 
-                        header("Content-Type: application/json;charset=utf-8");
-                        echo json_encode(["insert" => "successful", "data" =>  $result !== false ? "Đã lưu" : "Lưu"]);
-                } else {
-                        header("Content-Type: application/json;charset=utf-8");
-                        echo json_encode(["insert" => "fail"]);
-                }
         } catch (Exception $ex) {
                 print_r($ex->getMessage());
         } finally {
