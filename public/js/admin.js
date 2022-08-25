487102d9c7119789ccf0c5465a02f7ca32857190
Crom Borw <66833938+CromBorw2e36@users.noreply.github.com>
2022-08-25 09:14:52 +0700

    unknown

--- public/js/admin.js
+++ public/js/admin.js
@@ -7,28 +7,203 @@
   \****************************************/
 /***/ (() => {
 
-// (function() {
-//     let MyEditor;
-//     // data table
-//     jQuery(document).ready(function($) {
-//         ClassicEditor
-//             .create(document.querySelector('#editor'), {
-//                 placeholder: 'Nhấn vào đây và hãy viết mô tả chi tiết!',
-//             })
-//             .then(editor => {
-//                 const toolbarContainer = document.querySelector('#toolbar-container');
-//                 toolbarContainer.appendChild(editor.ui.view.toolbar.element);
-//                 MyEditor = editor;
-//             })
-//             .catch(error => {
-//                 console.error(error);
-//             });
-//     });
-//     // $.validator.addMethod("ck_editor", function() {
-//     //     var content_length = MyEditor.getData().trim().length;
-//     //     return content_length > 0;
-//     // }, "Please insert content for the page.");       
-// })(jQuery)
+(function () {
+  jQuery(document).ready(function ($) {
+    // tinymce editor
+    tinymce.init({
+      selector: '#mytextareapost',
+      plugins: 'image code',
+      // toolbar: 'undo redo | link image | code',
+
+      /* enable title field in the Image dialog*/
+      image_title: true,
+
+      /* enable automatic uploads of images represented by blob or data URIs*/
+      automatic_uploads: false,
+      height: "480",
+
+      /*
+        URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+        images_upload_url: 'postAcceptor.php',
+        here we add custom filepicker only to Image dialog
+      */
+      file_picker_types: 'image',
+      // images_upload_url: 'saveimg.php',
+      // images_upload_base_path: '../public/images/blogpost',
+      // images_upload_credentials: true,
+
+      /* and here's our custom image picker*/
+      file_picker_callback: function file_picker_callback(cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        /*
+          Note: In modern browsers input[type="file"] is functional without
+          even adding it to the DOM, but that might not be the case in some older
+          or quirky browsers like IE, so you might want to add it to the DOM
+          just in case, and visually hide it. And do not forget do remove it
+          once you do not need it anymore.
+        */
+
+        input.onchange = function () {
+          var file = this.files[0];
+          var reader = new FileReader();
+
+          reader.onload = function () {
+            /*
+              Note: Now we need to register the blob in TinyMCEs image blob
+              registry. In the next release this part hopefully won't be
+              necessary, as we are looking to handle it internally.
+            */
+            var id = 'BaiVietID' + new Date().getTime();
+            var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+            var base64 = reader.result.split(',')[1];
+            var blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
+            /* call the callback and populate the Title field with the file name */
+
+            cb(blobInfo.blobUri(), {
+              title: file.name
+            }); // lưu tên hình vào db
+          };
+
+          reader.readAsDataURL(file);
+        };
+
+        input.click();
+      },
+      content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    }); //add kind new
+
+    $("#addKind").on("click", function (event) {
+      event.preventDefault();
+      var modalKind = $("#kindModal");
+      modalKind.modal('show');
+      $("#themepost").focus();
+    }); //close the modal Kinđ
+
+    $(".closeKindModal").on("click", function () {
+      var modalKind = $("#kindModal");
+      modalKind.modal('hide');
+    }); //save the modal Kind
+
+    $("#btnSaveKind").on("click", function (event) {
+      //gọi ajax lưu lại bảng Kind
+      event.preventDefault();
+      var kindname = $("#themepost").val();
+
+      if (kindname.length > 0) {
+        $.ajax({
+          url: "../api/blogpost/savekindpost",
+          type: "post",
+          dataType: "text",
+          data: {
+            kind: kindname
+          }
+        });
+        $("#errorThemePostInput").html("");
+        $("#themepost").val('');
+        $("#kindModal").modal('hide');
+      } else {
+        $("#errorThemePostInput").html("Vui lòng nhập chủ đề bài viết");
+      }
+    }); //kind table edit
+
+    "use strict";
+
+    $("#kindtable").DataTable({
+      // processing: true,
+      // serverSide: true,
+      searchPanes: {
+        controls: false
+      },
+      searching: false,
+      info: false,
+      ajax: {
+        url: "../api/blogpost/getkindpost",
+        dataType: 'json',
+        type: 'get'
+      },
+      createdRow: function createdRow(row, data, dataIndex) {
+        if (data.status === 0) {
+          $(row).addClass('badge-light-danger');
+        }
+      },
+      columns: [{
+        "data": "id",
+        render: function render(data, type, row) {
+          return "<p class='counternumber'>Number</p>";
+        }
+      }, {
+        "data": "kindname",
+        render: function render(data, type, row) {
+          return "<input type=\"text\" name=\"kindname\" value=\"".concat(data, "\">");
+        }
+      }, {
+        "data": null,
+        render: function render(data, type, row) {
+          // return `<a href="?id=${data.id}" id="moreview">Xem thêm</a>`
+          return "<div class=\"text-center d-block\">\n                                <span id=\"edit\" class=\"material-symbols-rounded\" style=\"color: #42855B\"> done </span>\n                                <span id=\"remote\" class=\"material-symbols-rounded\" style=\"color: #D61C4E\"> delete </span>\n                            </div>"; // return `<button id="moreviewbutton" class="btn btn-success m-1 p-1">Xem</button>`;
+        }
+      }],
+      "order": [[1, 'asc']],
+      // initComplete: function(settings, json) {
+      //     InitLoadSuccess(settings, json);
+      // }
+      stateSave: true,
+      responsive: true,
+      aoColumnDefs: [{
+        bSortable: false,
+        aTargets: [0]
+      }],
+      orderCellsTop: true,
+      fixedHeader: true,
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.12.1/i18n/vi.json",
+        paginate: {
+          next: '»',
+          previous: '«'
+        }
+      }
+    }); // count number in table 
+
+    $("#kindtable").on("draw.dt", function () {
+      var n = 0;
+      $(".counternumber").each(function () {
+        $(this).html(++n);
+      });
+    }); //submit save post
+
+    tinyMCE.triggerSave();
+    $("#savepost").on("click", function (event) {
+      event.preventDefault();
+      var title = $("#titlepost").val();
+      var nameimage = $('#imagepost')[0].files[0].name;
+      var kind = $('input[name="radioKind"]:checked').val();
+      var data = tinyMCE.get('mytextareapost').getContent(); // console.log("data: " + data);
+      // $("div #data").html(data);
+
+      $.ajax({
+        type: "post",
+        url: "../api/blogpost/insertblog",
+        dataType: "text",
+        data: {
+          title: title,
+          content: data,
+          nameimage: nameimage,
+          kind: kind,
+          status: 0
+        } // cache: false,
+
+      }).done(function () {// console.log("data" + editorData);
+        // $("#data").html(data);
+        // alert(editorData);
+        // console.log("data: " + data);
+        // alert("title: " + title + ", nameimage: " + nameimage + ", kind: " + kind + ", data: " + data);
+      });
+    });
+  });
+})(jQuery);
 
 /***/ }),
 
@@ -56,7 +231,10 @@
         }
       },
       columns: [{
-        "data": 'id'
+        "data": "id",
+        render: function render(data, type, row) {
+          return "<p class='counternumber'>Number</p>";
+        }
       }, {
         "data": "fullname"
       }, {
@@ -114,6 +292,13 @@
           previous: '«'
         }
       }
+    }); // count number in table 
+
+    $("#contactstable").on("draw.dt", function () {
+      var n = 0;
+      $(".counternumber").each(function () {
+        $(this).html(++n);
+      });
     }); //Thao tác với bảng
 
     $('#contactstable tbody').on('click', 'tr', function () {
